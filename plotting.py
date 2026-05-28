61d5704600de55e5182e2c2555340952bcf57081
Arseniy Fedoseev <q.arseniyfedoseev@gmail.com>
2026-05-28 11:28:26 +0200

    Improved the visuals on our graph

--- plotting.py
+++ plotting.py
@@ -19,9 +19,27 @@ def plot_relative(points, density_matrix):
 
     phis = np.linspace(0, 2*np.pi, points)
     s = np.zeros(points)
+    phi_opt = np.pi / 8
+    S_opt = 2 * np.sqrt(2)
+
     for i, phi in enumerate(phis):
         s[i] = estimate_s(phi, density_matrix).real
     ax.plot(phis, s)
+
+    ax.axhline(y=-2, color='k', linestyle='--', label='Classical ±2')
+    ax.axhline(y=2, color='k', linestyle='--')
+    ax.axhline(y=-2 * np.sqrt(2), color='r', linestyle='--', label='Quantum ±2√2')
+    ax.axhline(y=2 * np.sqrt(2), color='r', linestyle='--')
+
+    plt.scatter(phi_opt, S_opt, color='r', s=80, zorder=5)
+    plt.annotate(
+        r"$\phi=\frac{\pi}{8},\quad S=2\sqrt{2}$",
+        xy=(np.pi / 8, 2 * np.sqrt(2)),
+        xytext=(0.75, 2.35),
+        arrowprops=dict(arrowstyle="->"),
+        bbox=dict(boxstyle="round,pad=0.3", fc="white", ec="black", alpha=0.9)
+    )
+    plt.legend(loc="right", framealpha=0.9)
     ax.set_xlabel(r'$\phi$, rad')
     ax.set_ylabel(r'$S$')
     ax.set_title(r'S as a function of $\phi$')
